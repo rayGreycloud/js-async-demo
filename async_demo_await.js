c96a558e9963f15b2fa340f1c317244e1185df58
rayGreycloud <rayGreycloud@gmail.com>
2017-07-29 08:32:33 -0700

    Refactor function using async and await keywords

---
+++ async_demo_await.js
@@ -0,0 +1,22 @@
+// Write a function to get json blob
+// Ajax request with fetch
+//  http://rallycoding.herokuapp.com/api/music_albums
+
+// Refactor using ES2017
+// Use async keyword
+async function fetchAlbums() {
+  // Add await before promises
+  // Assign to variables
+  const res = await fetch("http://rallycoding.herokuapp.com/api/music_albums");
+  const json = await res.json();
+
+  console.log(json);
+}
+
+// function fetchAlbums() {
+//   fetch("http://rallycoding.herokuapp.com/api/music_albums")
+//     .then(res => res.json())
+//     .then((json = console.log(json)));
+// }
+
+fetchAlbums();
